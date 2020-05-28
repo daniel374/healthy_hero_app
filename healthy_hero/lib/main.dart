4e2bda628dec715c7a55b3fe775db8000e69b367
daniel374 <damrodriguezlay@unal.edu.co>
2020-05-28 01:03:43 -0500

    ejemplo con post utilizando blocc y firestore, para posterior homologación con la colección de recetas

--- healthy_hero/lib/main.dart
+++ healthy_hero/lib/main.dart
@@ -1,49 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:healthy_hero/src/bloc/authentication_bloc/authentication_bloc.dart';
-import 'package:healthy_hero/src/bloc/authentication_bloc/authentication_event.dart';
-import 'package:healthy_hero/src/bloc/authentication_bloc/authentication_state.dart';
-import 'package:healthy_hero/src/repository/user_repository.dart';
+import 'package:healthy_hero/src/ui/home/home.dart';
 
-import 'src/bloc/avatars_bloc.dart/homePage.dart';
+void main() => runApp(BlogApp());
 
-void main() { 
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  final UserRepository userRepository = UserRepository();
-  
-  runApp(
-    BlocProvider(
-      create: (context) => AuthenticationBloc(userRepository: userRepository)
-      ..add(AppStarted()),
-      child: MyApp(userRepository: userRepository),
-    )
-  );
-
-}
-
-class MyApp extends StatelessWidget {
-  
-  final UserRepository _userRepository;
-
-  MyApp({Key key, @required UserRepository userRepository})
-   : assert (userRepository != null),
-     _userRepository = userRepository,
-     super(key: key);
-     
+class BlogApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My app Recetas",
+      title: "Blog App II",
       theme: ThemeData(
-        primarySwatch: Colors.orange
+        primarySwatch: Colors.red,
+        accentColor: Colors.redAccent
       ),
       debugShowCheckedModeBanner: false,
-      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-        builder: (context, state) {
-          return HomePage();
-        },
-      ),
+      home: Home(),
     );
   }
 }
